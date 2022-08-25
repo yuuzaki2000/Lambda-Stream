8415179ac37bbd0e940e39d4b92714946cbdb62a
MasanoriIkeda <107474470+yuuzaki2000@users.noreply.github.com>
2022-08-26 07:08:46 +0900

    新規Javaプロジェクト作成

---
+++ src/Chapter10.java
@@ -0,0 +1,29 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Chapter10 {
+	public static void main(String[] args) throws Exception {
+		List<Task> list = new ArrayList<>();
+		list.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う", true));
+		list.add(new Task(LocalDate.of(2021, 9, 15), "○○社面接", false));
+		list.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う", true));
+		list.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く", false));
+		list.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く", false));
+
+		// 以下記述
+		List<Task> filtered = new ArrayList<>();
+
+		for (Task task : list) {
+			if (task.isDone() == false) {
+				filtered.add(task);
+			}
+		}
+
+		System.out.println("未完了のタスクの個数は" + filtered.size());
+
+		System.out.println("【未完了のタスクを昇順に並び替えて一覧表示】");
+
+		list.stream().filter(f -> f.isDone() == false).sorted().forEach(System.out::println);
+	}
+}
---
+++ src/Task.java
@@ -0,0 +1,39 @@
+import java.time.LocalDate;
+
+//Comparableインタフェースを実装する
+public class Task implements Comparable<Task> {
+private LocalDate date;
+private String task;
+private boolean done; // 追加　完了はtrue, 未完了は false
+
+public Task(LocalDate date, String task, boolean done) {
+	this.date = date;
+	this.task = task;
+	this.done = done;
+}
+
+public LocalDate getDate() {
+	return date;
+}
+
+public String getTask() {
+	return task;
+}
+
+// boolean型のgetterメソッドはis～とすることもあります
+public boolean isDone() {
+	return done;
+}
+
+// compareToメソッドをオーバーライドする
+// 日付で並び替えるため、LocalDateのcompareToを使う
+@Override
+public int compareTo(Task t) {
+	return this.date.compareTo(t.date);
+}
+
+@Override
+public String toString() {
+	return this.date + " " + this.task;
+}
+}
